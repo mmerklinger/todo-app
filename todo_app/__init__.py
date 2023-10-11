73978b76276ee27d8107309ee88b90b591a5bc40
Markus Merklinger <markus.merklinger@code.berlin>
2023-10-11 19:24:52 +0200

    Add configuration module

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
+from .config import create_config
+
 
 def create_app() -> Flask:
-    return Flask(__name__)
+    app = Flask(__name__)
+    app.config = create_config(app.root_path)
+    return app
 
 
 app = create_app()
---
+++ todo_app/config.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+from os.path import exists
+
+from flask.config import Config
+
+
+class DefaultConfig(object):
+    DEBUG = False
+    TESTING = False
+    PROPAGATE_EXCEPTIONS = None
+    TRAP_HTTP_EXCEPTIONS = False
+    TRAP_BAD_REQUEST_ERRORS = None
+    SECRET_KEY = None
+    SESSION_COOKIE_NAME = "session"
+    SESSION_COOKIE_DOMAIN = None
+    SESSION_COOKIE_PATH = None
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = False
+    SESSION_COOKIE_SAMESITE = None
+    PERMANENT_SESSION_LIFETIME = timedelta(days=31)
+    SESSION_REFRESH_EACH_REQUEST = True
+    USE_X_SENDFILE = False
+    SEND_FILE_MAX_AGE_DEFAULT = None
+    SERVER_NAME = None
+    APPLICATION_ROOT = "/"
+    PREFERRED_URL_SCHEME = "http"
+    MAX_CONTENT_LENGTH = None
+    TEMPLATES_AUTO_RELOAD = None
+    EXPLAIN_TEMPLATE_LOADING = False
+
+
+def create_config(root_path: str) -> Config:
+    config = Config(root_path)
+    config.from_object(DefaultConfig())
+    if exists(".env"):
+        config.from_pyfile(".env", silent=True)
+    else:
+        config.from_prefixed_env()
+    return config

0abb2c316f7bf79d24951ba99fe37394728b942f
Markus Merklinger <markus.merklinger@code.berlin>
2023-10-13 13:07:31 +0200

    Add db initialization to flask application object

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 
 from .config import create_config
+from .db import create_db
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config = create_config(app.root_path)
+
+    db = create_db()
+    db.init_app(app)
+
     return app
 
 

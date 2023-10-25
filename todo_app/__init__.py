a4cdb5941186be9966d2756d11b66b4ce6292264
Markus Merklinger <markus.merklinger@code.berlin>
2023-10-25 11:40:43 +0200

    Initialize db on the package level

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -4,12 +4,13 @@ from todo_app.config import create_config
 from todo_app.db import create_db
 from todo_app.views import bp_root
 
+db = create_db()
+
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config = create_config(app.root_path)
 
-    db = create_db()
     db.init_app(app)
     with app.app_context():
         db.create_all()

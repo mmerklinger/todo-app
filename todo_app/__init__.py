e6a8a3c337a3ccfc210f69de277012314d6d5b0d
Markus Merklinger <markus.merklinger@code.berlin>
2023-10-25 12:33:57 +0200

    Make blueprints a dependency

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -1,25 +1,31 @@
-from flask import Flask
+from typing import List
+
+from flask import Blueprint, Flask
 from flask.config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 from todo_app.config import create_config
 from todo_app.db import create_db
-from todo_app.views import bp_root
 
 config = create_config(__path__[0])
 db = create_db()
 
+from todo_app.views import bp_root
 
-def create_app(config: Config, db: SQLAlchemy) -> Flask:
+blueprints = [bp_root]
+
+
+def create_app(config: Config, db: SQLAlchemy, blueprints: List[Blueprint]) -> Flask:
     app = Flask(__name__)
     app.config = config
 
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.register_blueprint(bp_root)
+    for bp in blueprints:
+        app.register_blueprint(bp)
 
     return app
 
 
-app = create_app(config, db)
+app = create_app(config, db, blueprints)

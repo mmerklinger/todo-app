1ae651411e384091af0bb379a9a9559b0242cf45
Markus Merklinger <markus.merklinger@code.berlin>
2023-11-14 14:53:36 +0100

    Add task list

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template
 from flask.typing import ResponseReturnValue
 
+from todo_app import db
+from todo_app.db import Tasks
+
 bp_root = Blueprint("root", __name__, url_prefix="/")
 bp_tasks = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -8,3 +11,9 @@ bp_tasks = Blueprint("tasks", __name__, url_prefix="/tasks")
 @bp_root.route("/")
 def index() -> ResponseReturnValue:
     return render_template("index.html")
+
+
+@bp_tasks.route("/", methods=["GET"])
+def get() -> ResponseReturnValue:
+    tasks = db.session.execute(db.select(Tasks)).scalars()
+    return render_template("tasks_get.html", tasks=tasks)

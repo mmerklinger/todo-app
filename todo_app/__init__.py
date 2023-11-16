0c5b4d520e2f7a0225657e0e9d84655b6a5640cb
Markus Merklinger <markus.merklinger@code.berlin>
2023-11-16 12:35:46 +0100

    Add blueprint for authentication

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -5,8 +5,8 @@ from todo_app.db import create_db
 config = create_config(__path__[0])
 db = create_db()
 
-from todo_app.views import bp_root, bp_tasks
+from todo_app.views import bp_auth, bp_root, bp_tasks
 
-blueprints = [bp_root, bp_tasks]
+blueprints = [bp_root, bp_tasks, bp_auth]
 
 app = create_app(config, db, blueprints)

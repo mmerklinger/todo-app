137916f47b0c178ff1afa41afa5dab28c6263749
Markus Merklinger <markus.merklinger@code.berlin>
2024-04-01 11:56:06 +0200

    Rename CSS class sidebar to menu

--- todo_app/static/script.js
+++ todo_app/static/script.js
@@ -1,12 +1,12 @@
 function toggleMenu() {
     const content = document.querySelector('.content');
-    const sidebar = document.querySelector('.sidebar');
+    const menu = document.querySelector('.menu');
 
-    if (sidebar.style.display === 'inline') { // sidebar visible
-        sidebar.style.display = 'none';
+    if (menu.style.display === 'inline') { // menu visible
+        menu.style.display = 'none';
         content.style.display = 'inline';
     } else { // content visible
         content.style.display = 'none';
-        sidebar.style.display = 'inline';
+        menu.style.display = 'inline';
     }
 }
